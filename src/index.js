17117fbad3e86d50d146898be46cc4c68d64b26d
abigaleypc <abigaleypc@gmail.com>
2018-03-06 22:27:04 +0800

    😆@babel/preset-react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-console.log('------------------------------------');
-console.log("haha");
-console.log('------------------------------------');
+import React from 'react'
+import ReactDOM from 'react-dom';
+import HelloMessage from './HelloMessage.jsx'
+
+ReactDOM.render(
+  <HelloMessage name="dd" />,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      { test: /\.jsx?$/, use: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  devtool: 'source-map',
+  mode: 'development'
+}
