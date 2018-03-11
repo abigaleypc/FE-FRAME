e8af6f6ddcf6dc0a62cc08ad5fc7290fb52b43b2
abigaleypc <abigaleypc@gmail.com>
2018-03-11 15:55:27 +0800

    layout

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Routers from './routers/index.jsx'
 import { Router } from 'react-router'
 import createBrowserHistory from 'history/createBrowserHistory'
-// TO DO : IMPORT ANTD CSS
 import 'antd/dist/antd.less';
 
 
 const history = createBrowserHistory()
 
-import Layout from "./containers/Layout/Layout";
+import BasicLayout from "./containers/BasicLayout/BasicLayout";
 
 const App = () => (
   <Router history={history}>
     <div>
-      <Layout />
-      <Routers />
+      <BasicLayout />
     </div>
   </Router>
 )
